531556eabe1733e20a29652a92a69bb2017351b6
catfistori <cat.fistori@gmail.com>
2015-08-29 16:25:13 -0700

    Added supporting pages

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* Enable smooth scrolling on all links with anchors */
+$('#nav.smoothscroll ul li a[href^="#"]').on('click', function(e) {
+
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  // animate
+  $('html, body').animate({
+    scrollTop: $('a[name="' + this.hash.replace('#', '') + '"]').offset().top
+  }, 700, function(){
+
+    // when done, add hash to url
+    // (default click behaviour)
+    window.location.hash = hash;
+
+  });
+});
